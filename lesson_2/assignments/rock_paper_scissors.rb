65d0f885be547336f9f908da27dee1848f3b0de9
gio-tessitore <giuseppetessitore3@gmail.com>
2020-05-19 22:11:09 +0200

    rock paper scissors lizard spock game

--- lesson_2/assignments/rock_paper_scissors.rb
+++ lesson_2/assignments/rock_paper_scissors.rb
@@ -1,13 +1,58 @@
 # frozen_string_literal: true
-VALID_CHOICES = %w[rock paper scissors].freeze
+VALID_CHOICES = %w[rock paper scissors spock lizard].freeze
 
 def prompt(message)
-  Kernel.puts("=> #{message}")
+  Kernel.puts("\n=> #{message}")
 end
 
+# this method serves to print_options method to add spaces and symbols
+def print_with_prefix(ra)
+  ra.map { |i| " #{[i].join('')}   -" }.join('   ')
+end
+
+# I created this method because I wanted to print all
+# the options for the game on one line.
+# The method iterates through the argument array,
+# takes the first letter (initial) of each string element
+# of the array and saves it in a new array. If the first letter of the
+# next element is already present in the new array the
+# iteration captures the first and second letter.
+# While the iteration do this, the method also
+# associates with each initial/s, a second element
+# which is the string 'for' and a third element
+# which is the whole string that the iteration took the initial from.
+
+def print_options(ra)
+  list_of_options = []
+  second_term = ' for ' 
+  new_ra = []
+  index1 = 0 
+  index2 = 0 
+  i = 0
+  ra.each do
+    new_ra << if new_ra.include?(ra[index1][index2])
+                ra[index1][index2] + ra[index1][index2 + 1]
+              else
+                ra[index1][index2]
+              end
+    index1 += 1
+    list_of_options << [new_ra[i], second_term, ra[i]]
+    i += 1
+  end
+  prompt("TYPE: #{print_with_prefix(list_of_options)}")
+end
+
+# making the variable accessible
+user_choice = ''
+
+# this method holds the rule of the game and
+# grabs the user and the computer values and decides who wins
 def who_wins(user, computer)
   who_wins = [%w[rock scissors], %w[paper rock],
-              %w[scissors paper]]
+              %w[scissors paper], %w[rock lizard],
+              %w[lizard spock], %w[spock scissors],
+              %w[scissors lizard], %w[lizard paper],
+              %w[paper spock], %w[spock rock], %w[rock scissors]]
 
   if who_wins.include?([user, computer])
     user
@@ -18,30 +63,79 @@ def who_wins(user, computer)
   end
 end
 
-loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = Kernel.gets.chomp
+# this is the initial prompt
+prompt("Welcome to
+   Rock, Paper, Scissors, Spock, Lizard! This is a five-set match.
+   The first of us that reaches 5 points will win the match.
 
-    if VALID_CHOICES.include?(choice)
-      prompt("Your choice is #{choice}")
-      break
-    else
-      prompt('This is not a valid choice!')
+   Let's play!
+
+   Choose among these elements: #{VALID_CHOICES.join(', ')}")
+
+# This is the outer loop. At the end it will ask you if you want to play again.
+loop do
+  choice = 0
+  user_points = 0
+  computer_points = 0
+
+  # this is the middle loop. it counts the points
+  loop do
+    break if user_points == 5 || computer_points == 5
+
+    # this is the inner loop. it checks if the input is valid
+    loop do
+      print_options(VALID_CHOICES)
+      choice = Kernel.gets.chomp
+
+      # grabs the initial/s a user inputs
+      # and translates it in one of the elements in the array
+      case choice
+      when 'r'
+        user_choice = 'rock'
+      when 'p'
+        user_choice = 'paper'
+      when 's'
+        user_choice = 'scissors'
+      when 'sp'
+        user_choice = 'spock'
+      when 'l'
+        user_choice = 'lizard'
+      end
+
+      if VALID_CHOICES.include?(user_choice)
+        prompt("Your choice is #{user_choice}")
+        break
+      else
+        prompt('This is not a valid choice!')
+      end
     end
+
+    # it is the computer turn to play
+    computer_choice = VALID_CHOICES.sample
+    prompt("Computer chose #{computer_choice}")
+
+    # here we add the points
+    if who_wins(user_choice, computer_choice) == user_choice
+      user_points += 1
+
+    elsif who_wins(user_choice, computer_choice) == computer_choice
+      computer_points += 1
+    else
+      prompt('It´s a tie')
+    end
+
+    # it prompts the result of each set
+    prompt("You have #{user_points} point/s")
+    prompt("Computer has #{computer_points} point/s")
   end
 
-  computer_choice = VALID_CHOICES.sample
-  prompt("Computer chose #{computer_choice}")
+  # it prompts the final result
+  if user_points == 5
+    prompt("Congratulations! You won the match.")
 
-  # Walk-through: Rock Paper Scissors question number 3
-  if who_wins(choice, computer_choice) == choice
-    prompt('You win!')
-  elsif who_wins(choice, computer_choice) == computer_choice
-    prompt('Sorry, computer wins!')
-  else
-    prompt('It´s a tie')
+  elsif computer_points == 5
+    prompt("Sorry, computer won the match.")
+
   end
 
   prompt("would you like to play again? ['Y', 'n']")
